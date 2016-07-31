8a218da38eb998bf06b06e2ae6131f60293a246f
Your Name <you@example.com>
2016-07-30 19:49:20 -0500

    Update setup to actually work now

--- setup.sh
+++ setup.sh
@@ -7,11 +7,10 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $DIR
 
-ln -s "$SOURCE" .vim
-ln -s "$SOURCE"/vimrc .vimrc
+ln -s "$DIR" "$HOME/.vim"
+ln -s "$DIR"/vimrc "$HOME/.vimrc"
 
 git submodule update --init --recursive
-vim -c 'PluginInstall'
-python ~/.vim/bundle/YouCompleteMe/install.py
+#vim -c 'PluginInstall'
+#python ~/.vim/bundle/YouCompleteMe/install.py
