d60eb40458324af2742e5c010766991ca4d28e25
Sean Wilson <spwilson27@gmail.com>
2016-08-19 10:42:01 -0500

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+set -ex
+
 SOURCE="${BASH_SOURCE[0]}"
+
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
 	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 	SOURCE="$(readlink "$SOURCE")"
 	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
+
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-ln -s "$DIR" "$HOME/.vim"
-ln -s "$DIR"/vimrc "$HOME/.vimrc"
+set +e
 
-git submodule update --init --recursive
-vim -c 'PluginInstall'
-python ~/.vim/bundle/YouCompleteMe/install.py
+ln -s "$DIR" "$HOME/.vim"
+ln -s "$DIR/vimrc" "$HOME/.vimrc"
+
+git pull; git submodule update --init --recursive
+
+#vim +PluginInstall +qall
+#python ~/.vim/bundle/YouCompleteMe/install.py
